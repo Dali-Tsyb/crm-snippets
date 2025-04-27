f2ec42bd1a421c7e931af18455833eca034ccdf0
Dali <satinktten@gmail.com>
2025-04-27 03:00:40 +0300

    fix build

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,13 @@ import MainPageView from "@/views/MainPageView.vue";
 import DashboardView from "@/views/DashboardView.vue";
 import DepartmentsView from "@/views/DepartmentsView.vue";
 
-const routes = [
-   { path: "/", component: MainPageView },
-   { path: "/dashboard", component: DashboardView },
-   { path: "/departments", component: DepartmentsView },
-];
-
-export default createRouter({
-   history: createWebHistory(),
-   routes,
+const router = createRouter({
+   history: createWebHistory(import.meta.env.BASE_URL),
+   routes: [
+      { path: "/", component: MainPageView },
+      { path: "/dashboard", component: DashboardView },
+      { path: "/departments", component: DepartmentsView },
+   ],
 });
+
+export default router;
